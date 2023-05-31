5dc1ee88639a04641406736bf110145dfe24afb7
nttungvn <tungntvn90@gmail.com>
2023-05-31 23:12:24 +0700

    Update customer_landing_trusted.py

--- customer_landing_trusted.py
+++ customer_landing_trusted.py
@@ -13,27 +13,22 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Amazon S3
-AmazonS3_node1685527754956 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://tungnt-stedi-lakehouse/customer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="AmazonS3_node1685527754956",
+# Script generated for node Customer landing
+Customerlanding_node1685547260568 = glueContext.create_dynamic_frame.from_catalog(
+    database="tungnt-stedi",
+    table_name="customer_landing",
+    transformation_ctx="Customerlanding_node1685547260568",
 )
 
 # Script generated for node Filter
 Filter_node1685527794372 = Filter.apply(
-    frame=AmazonS3_node1685527754956,
+    frame=Customerlanding_node1685547260568,
     f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
     transformation_ctx="Filter_node1685527794372",
 )
 
-# Script generated for node Amazon S3
-AmazonS3_node1685527813131 = glueContext.write_dynamic_frame.from_options(
+# Script generated for node Customer trusted
+Customertrusted_node1685546596491 = glueContext.write_dynamic_frame.from_options(
     frame=Filter_node1685527794372,
     connection_type="s3",
     format="json",
@@ -41,7 +36,7 @@ AmazonS3_node1685527813131 = glueContext.write_dynamic_frame.from_options(
         "path": "s3://tungnt-stedi-lakehouse/customer/trusted/",
         "partitionKeys": [],
     },
-    transformation_ctx="AmazonS3_node1685527813131",
+    transformation_ctx="Customertrusted_node1685546596491",
 )
 
 job.commit()
