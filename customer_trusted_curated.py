fe9c1fa50c1216eac543e4925b89c9d9a8acc451
nttungvn <tungntvn90@gmail.com>
2023-05-31 23:13:19 +0700

    Update customer_trusted_curated.py

--- customer_trusted_curated.py
+++ customer_trusted_curated.py
@@ -14,43 +14,33 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Customer trusted
-Customertrusted_node1685542253552 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://tungnt-stedi-lakehouse/customer/trusted/"],
-        "recurse": True,
-    },
-    transformation_ctx="Customertrusted_node1685542253552",
+# Script generated for node Accelerometer landing
+Accelerometerlanding_node1685547868660 = glueContext.create_dynamic_frame.from_catalog(
+    database="tungnt-stedi",
+    table_name="accelerometer_landing",
+    transformation_ctx="Accelerometerlanding_node1685547868660",
 )
 
-# Script generated for node Accelerometer landing
-Accelerometerlanding_node1685542271874 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://tungnt-stedi-lakehouse/accelerometer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="Accelerometerlanding_node1685542271874",
+# Script generated for node Customer trusted
+Customertrusted_node1685547857013 = glueContext.create_dynamic_frame.from_catalog(
+    database="tungnt-stedi",
+    table_name="customer_trusted",
+    transformation_ctx="Customertrusted_node1685547857013",
 )
 
 # Script generated for node Join
 Join_node1685542293514 = Join.apply(
-    frame1=Customertrusted_node1685542253552,
-    frame2=Accelerometerlanding_node1685542271874,
-    keys1=["email"],
-    keys2=["user"],
+    frame1=Accelerometerlanding_node1685547868660,
+    frame2=Customertrusted_node1685547857013,
+    keys1=["user"],
+    keys2=["email"],
     transformation_ctx="Join_node1685542293514",
 )
 
 # Script generated for node Drop Fields
 DropFields_node1685542332808 = DropFields.apply(
     frame=Join_node1685542293514,
-    paths=["y", "z", "timeStamp", "user", "x"],
+    paths=["y", "z", "user", "x", "timestamp"],
     transformation_ctx="DropFields_node1685542332808",
 )
 
