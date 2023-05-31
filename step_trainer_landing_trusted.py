bda2a48fc31cb533a2774762fdafd52f3dcbbee5
nttungvn <tungntvn90@gmail.com>
2023-05-31 23:13:48 +0700

    Update step_trainer_landing_trusted.py

--- step_trainer_landing_trusted.py
+++ step_trainer_landing_trusted.py
@@ -12,36 +12,26 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Customer curated
-Customercurated_node1685543749894 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://tungnt-stedi-lakehouse/customer/curated/"],
-        "recurse": True,
-    },
-    transformation_ctx="Customercurated_node1685543749894",
+# Script generated for node Step trainer landing
+Steptrainerlanding_node1685548228653 = glueContext.create_dynamic_frame.from_catalog(
+    database="tungnt-stedi",
+    table_name="step_trainer_landing",
+    transformation_ctx="Steptrainerlanding_node1685548228653",
 )
 
-# Script generated for node Step trainer landing
-Steptrainerlanding_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://tungnt-stedi-lakehouse/step_trainer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="Steptrainerlanding_node1",
+# Script generated for node Customer curated
+Customercurated_node1685549266394 = glueContext.create_dynamic_frame.from_catalog(
+    database="tungnt-stedi",
+    table_name="customer_curated",
+    transformation_ctx="Customercurated_node1685549266394",
 )
 
 # Script generated for node Join
 Join_node1685543780008 = Join.apply(
-    frame1=Customercurated_node1685543749894,
-    frame2=Steptrainerlanding_node1,
-    keys1=["serialNumber"],
-    keys2=["serialNumber"],
+    frame1=Steptrainerlanding_node1685548228653,
+    frame2=Customercurated_node1685549266394,
+    keys1=["serialnumber"],
+    keys2=["serialnumber"],
     transformation_ctx="Join_node1685543780008",
 )
 
@@ -49,15 +39,15 @@ Join_node1685543780008 = Join.apply(
 DropFields_node1685544222066 = DropFields.apply(
     frame=Join_node1685543780008,
     paths=[
-        "birthDay",
-        "shareWithResearchAsOfDate",
-        "registrationDate",
-        "customerName",
         "email",
-        "lastUpdateDate",
         "phone",
-        "shareWithPublicAsOfDate",
-        "shareWithFriendsAsOfDate",
+        "sharewithpublicasofdate",
+        "birthday",
+        "registrationdate",
+        "sharewithresearchasofdate",
+        "customername",
+        "lastupdatedate",
+        "sharewithfriendsasofdate",
     ],
     transformation_ctx="DropFields_node1685544222066",
 )
